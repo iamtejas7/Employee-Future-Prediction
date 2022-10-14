ef7c932d295cc54f8f3588d0f2794af18d6da51e
Tejas Bankar <iamtejas7@gmail.com>
2022-10-14 13:15:55 +0530

    flask web application

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 def employee():
     return render_template('employee.html')
 
-@app.route('/predict_api', methods = ['POST'])
 
+@app.route('/predict_api', methods = ['POST'])
 def predict_api():
     data = request.json['data']
     features_list = ['Education', 'JoiningYear', 'City', 'PaymentTier', 'Age', 'Gender','EverBenched', 'ExperienceInCurrentDomain']
@@ -38,6 +38,43 @@ def predict_api():
         return "This Employee will not leave company"
     else:
         return "This Employee is likely to leave company"
+
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    data = request.form
+    input_values = list(data.values())
+    user_features = []
+    for i in input_values:
+        try:
+            user_features.append(eval(i))
+        except:
+            user_features.append(i)
+    # print(user_features)
+
+    education = {'Bachelors': 0, 'Masters': 1, 'PHD': 2}
+    city = {'Bangalore': 0, 'Pune': 1, 'New Delhi': 2}
+    gender = {'Male': 0, 'Female': 1}
+    ever_bench = {'No': 0, 'Yes': 1}
+
+    try:
+        user_features[0] = education[user_features[0]]
+        user_features[2] = city[user_features[2]]
+        user_features[5] = gender[user_features[5]]
+        user_features[6] = ever_bench[user_features[6]]
+        # print(user_features)
+    except:
+        return render_template("employee.html", prediction_text = "Please fill all boxes with correct values")
+
+    else:
+        new_data = scaler.transform(np.array(user_features).reshape(1,-1))
+        output = rf_model.predict(new_data)
+        # print(output)
+
+        if output[0] == 0:
+            return render_template("employee.html", prediction_text = "This Employee will not leave company")
+        else:
+            return render_template("employee.html", prediction_text = "This Employee is likely to leave the company")
 
 
 if __name__ == '__main__':
